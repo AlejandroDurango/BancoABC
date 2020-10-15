fbbdb8eec97c01096e358b9615f4f8f420357b7e
AlejandroDurango <69972787+AlejandroDurango@users.noreply.github.com>
2020-10-14 23:31:38 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoABC
+{
+    class Principal
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
