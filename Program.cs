1cc05eb23e7adda872dc80889260ef2bc57ab3d2
AlejandroDurango <69972787+AlejandroDurango@users.noreply.github.com>
2020-10-21 00:20:07 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BancoABC
+{
+    class Principal
+    {
+        static void Main(string[] args)
+        {   Form IniciarApp = new Menu_principal();
+            Application.Run(IniciarApp);
+        }
+
+
+    }
+}
